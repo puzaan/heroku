a1d6d073858e845b71637b4f1f7085ea7282f20d
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-07-14 10:45:59 +0545

    : aded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello Server is running')
+    res.send('Hello: Server is running')
 });
 
 const PORT =process.env.PORT || 5000
